c4977bd5fde1c50c8d79885cb8f69ac85da8614b
Compey <hi@devcomp.xyz>
2022-11-19 22:39:20 +0530

    feat: working ascii & gradient

--- packages/cli/lib/constants.ts
+++ packages/cli/lib/constants.ts
@@ -1,14 +1,28 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import { cwd } from 'process'
-import * as http from 'http'
+import * as https from 'https'
+import axios from 'axios'
+import kleur from 'kleur'
+import gradient from 'gradient-string'
+
 
 export const c = {
-    // Welcome message is only accessable by files in src, 
+    // Welcome message is only accessable by files in lib, 
     // else it throws an error.
-    welcomeMessage: `${http.get("https://raw.githubusercontent.com/CompeyDev/create-guilded-bot/main/packages/lib/welcome.txt")}`
-}
+    word_CreateAscii: "                      _       \n                     | |      \n   ___ _ __ ___  __ _| |_ ___ \n  / __| \'__/ _ \\/ _` | __/ _ \\\n | (__| | |  __/ (_| | ||  __/\n  \\___|_|  \\___|\\__,_|\\__\\___|",
+    word_GuildedAscii: "              _ _     _          _ \n             (_) |   | |        | |\n   __ _ _   _ _| | __| | ___  __| |\n  / _` | | | | | |/ _` |/ _ \\/ _` |\n | (_| | |_| | | | (_| |  __/ (_| |\n  \\__, |\\__,_|_|_|\\__,_|\\___|\\__,_|\n   __/ |                           \n  |___/             ",
+    word_BotAscii: "  _           _   \n | |         | |  \n | |__   ___ | |_ \n | \'_ \\ / _ \\| __|\n | |_) | (_) | |_ \n |_.__/ \\___/ \\__|",
+    word_HyphenAscii: "         \n         \n  ______ \n |______|\n         \n         \n         \n         ",
+    welcomeMessage: gradient.rainbow.multiline("                      _                         _ _     _          _                         \n                     | |                       (_) |   | |        | |                        \n   ___ _ __ ___  __ _| |_ ___ ______ __ _ _   _ _| | __| | ___  __| |______ __ _ _ __  _ __  \n  / __| \'__/ _ \\/ _` | __/ _ \\______/ _` | | | | | |/ _` |/ _ \\/ _` |______/ _` | \'_ \\| \'_ \\ \n | (__| | |  __/ (_| | ||  __/     | (_| | |_| | | | (_| |  __/ (_| |     | (_| | |_) | |_) |\n  \\___|_|  \\___|\\__,_|\\__\\___|      \\__, |\\__,_|_|_|\\__,_|\\___|\\__,_|      \\__,_| .__/| .__/ \n                                     __/ |                                      | |   | |    \n                                    |___/                                       |_|   |_|   \n")
 
-export default function getConstant(constant: string): string {
+} 
+
+const hyphen = kleur.yellow(c.word_HyphenAscii)
+const styleGreen = kleur.green
+
+// c.welcomeMessage = styleGreen(c.word_CreateAscii.trimEnd()) + hyphen + styleGreen(c.word_GuildedAscii.trimEnd()) + styleGreen(c.word_BotAscii.trimEnd())
+
+export default function getConstant(constant: keyof typeof c): string {
     return c[constant]
 }
